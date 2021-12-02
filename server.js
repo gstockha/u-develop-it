e0428445197c1843c4a542690db06ffbb1cd1ca4
George Stockhammer <sunchipzzz@gmail.com>
2021-12-02 13:31:51 -0600

    made routes and added voter routes

--- server.js
+++ server.js
@@ -1,122 +1,27 @@
-const mysql = require('mysql2');
 const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const inputCheck = require('./utils/inputCheck');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'password',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
-// Get all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name
-    AS party_name 
-    FROM candidates 
-    LEFT JOIN parties 
-    ON candidates.party_id = parties.id`;
-    //inorder: select from candidates table with all their keys, and the parties table with name of parties
-    //AS party_name will call 'name' party_name on the graph instead
-    //FROM candidates makes that table our focus to draw onto
-    //LEFT JOIN means we're merging (parts) of the parties table onto candidates
-    //ON candidates.party_id = parties.id means we're matching candidate's party_id's with parties' id's
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-// Get a single candidate
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-    AS party_name 
-    FROM candidates 
-    LEFT JOIN parties 
-    ON candidates.party_id = parties.id 
-    WHERE candidates.id = ?`; //have to place where at the end of the statement if doing a join
-    const params = [req.params.id]; //sql query takes params as an array
-  
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-});
-// Delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-  
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.statusMessage(400).json({ error: res.message });
-      } else if (!result.affectedRows) { //if affected rows = 0 (false)
-        res.json({
-          message: 'Candidate not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-});
-// Create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    //{ body } is the samething as saying const body = res.body
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-      res.status(400).json({ error: errors });
-      return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
 
-//routes
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
-//server
-app.listen(PORT, () => {
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  });
 });
